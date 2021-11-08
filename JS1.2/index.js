9e5efb48cfcb03d06f57f2f8ee25040458f87a2d
matimas <mati_shak@walla.com>
2021-11-08 19:42:03 +0200

    com

---
+++ JS1.2/index.js
@@ -0,0 +1,72 @@
+/*
+--Delete the wrong answers--
+
+1. Which of the following is/are strings?
+
+c) '4'
+
+2. Which of the following is/are numbers?
+
+a) 4
+b) 4.0
+d) -4
+
+3. Which of the following is/are booleans?
+
+a) true
+b) false
+
+4. What is the result of 80 + 71.2?
+
+a) 151.2/151
+
+5. What is the result of "80" + 71.2?
+
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+
+a) number
+
+7. Does "100" + 30 produce a number or a string?
+
+b)string
+
+*/
+
+//------Submit your answers under the question------//
+
+//create three different ways to declare variables
+// let x = 0;
+// const y = 0;
+// var z = 0;
+
+//declare a variable and reassign a value to it
+// let x = 5;
+// x = 10;
+
+//create a variable and after that assign a string with its value being: He's got it!
+// let y = ""
+// y = "i hope i understand";
+
+/*
+1. create a variable and assign a value on how much a restaurant bill is.
+
+let rest_bill = 151.5;
+
+2. create a variable and assign a value on how much tax they should pay.
+
+let tex2pay = (180*(tax))
+
+3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
+
+let total_bill = console.log("Your tota bill:" + (tax2pay + rest_bill))
+ */
+
+// Round the number 50.6 to its nearest integer
+
+//math.round(50.6);
+
+//Create a variable that is undefined
+
+// let und
