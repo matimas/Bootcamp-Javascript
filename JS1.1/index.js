1cd980ba14e301c853b3a5f16ed0e7fa625ed46a
matimas <mati_shak@walla.com>
2021-11-08 19:38:25 +0200

    com

---
+++ JS1.1/index.js
@@ -0,0 +1,18 @@
+let mystring = 'asdsa';
+let mynum = 14;
+const mati = {
+	name: 'mati',
+	age: 33,
+	singel: true,
+};
+let is_my_real_num = true;
+let empty = null;
+let mySym = Symbol('work-hard');
+let und = 'undefine';
+console.log(mystring);
+console.log(mynum);
+console.log(mati);
+console.log(is_my_real_num);
+console.log(empty);
+console.log(mySym);
+console.log(und);
