db34d7bb5c9e91e8febab49ca1bb050b7ba33024
matimas <mati_shak@walla.com>
2021-11-08 19:45:45 +0200

    com

---
+++ JS4.1/index.js
@@ -0,0 +1,11 @@
+let numOfSiblings = parseInt(prompt('How many siblings do you have?', 0));
+if (numOfSiblings === 1) {
+	console.log('1 Siblings!');
+} else if (numOfSiblings > 1) {
+	console.log('More than 1 Sibling');
+} else {
+	console.log('NO Siblings');
+}
+// i think, the 3 equals operator is for deeping intro the type of the object and not only the value is represent
+// please note me if im wrong
+//בטוח אני טועה באנגלתי אבל אמבטיח להשתפר
