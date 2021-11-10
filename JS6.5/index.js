cd10a0fbbc0a3001db1542e3b841181f6bbb3710
mati misganow <45734976+matimas@users.noreply.github.com>
2021-11-10 10:05:19 +0200

    Add files via upload

---
+++ JS6.5/index.js
@@ -0,0 +1,33 @@
+//fill an arry with
+const array1 = new Array(100);
+array1.fill(100);
+console.log(array1);
+//creating an arry with Array.from
+console.log(Array.from([10, 20, 30], (x) => x + x));
+const person = {
+	firstName: 'John',
+	lastName: 'Doe',
+};
+
+//valus of object into an array
+const pValues = Object.values(person);
+console.log(pValues);
+
+array1.push(pValues);
+
+//im object with array prop
+const myObj = {
+	my_arr: array1,
+};
+//im array?
+if (array1.length) {
+	console.log('im array');
+}
+//new array with spred ope, not effect
+const array3 = [...array1];
+
+console.log(array3);
+console.log(array1);
+
+//copy that effect
+const array4 = array3;
