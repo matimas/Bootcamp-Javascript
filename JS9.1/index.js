6729ac2e8e7eae36e66cf6798782c6fdf0eb1ca5
mati misganow <45734976+matimas@users.noreply.github.com>
2021-11-14 15:10:04 +0200

    Add files via upload

---
+++ JS9.1/index.js
@@ -0,0 +1,14 @@
+const popV = ['12132', '127653', '128756734', '12345'];
+
+const populationPercentages = (arr) => {
+	let percentages = [];
+
+	let i = 0;
+	while (i < arr.length) {
+		percentages.push(arr[i].length);
+		i++;
+	}
+	return percentages;
+};
+
+console.log(populationPercentages(popV));
