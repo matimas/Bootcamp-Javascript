a5e8bc03cc8b4498e203be5ebd672e6decb19771
mati misganow <45734976+matimas@users.noreply.github.com>
2021-11-16 00:19:11 +0200

    Add files via upload

---
+++ JS12.2/index.js
@@ -0,0 +1,38 @@
+const candyStore = {
+	candies: [
+		{
+			id: 'as12f',
+			name: 'mint gum',
+			price: 2,
+			amount: 2,
+		},
+		{
+			id: '5hd7y',
+			name: 'twix',
+			price: 5,
+			amount: 4,
+		},
+	],
+	cashRegister: 200,
+};
+
+const getCandy = (candyStore, id) => {
+	const candi = candyStore.candies.filter((candy) => candy.id === id);
+	return candy[0];
+};
+
+const getPrice = (candyStore, id, name, price, amount) => {
+	candyStore.candies.push({ name: name, id: id, price: price, amount: amount });
+	console.log(candyStore.candies);
+};
+
+const buy = (candyStore, id) => {
+	candyStore.candies.forEach((candy) => {
+		if (candy.id === id) {
+			candy.amount--;
+			candyStore.cashRegister += candy.price;
+		}
+	});
+	console.log(candyStore);
+};
+buy(candyStore, '5hd7y');
