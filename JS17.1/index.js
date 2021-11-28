b4c07f373b32dd1fe102b8982efd801427b685cc
mati misganow <45734976+matimas@users.noreply.github.com>
2021-11-28 10:56:32 +0200

    Add files via upload

--- JS17.1/index.js
+++ JS17.1/index.js
@@ -1,27 +1,33 @@
+// Block 1
+var b = 1;
+
 function someFunction(number) {
-	function otherFunction(input) {
-		return b;
-	}
-	b = 5;
-	return otherFunction;
+  function otherFunction(input) {
+    return b;
+  }
+  b = 5;
+  return otherFunction;
 }
 var firstResult = someFunction(9);
 var result = firstResult(2);
-console.log(result);
+console.log(result); // 5
 
+// Block 2
 var a = 1;
 function b2() {
-	a = 10;
-	return;
-	function a() {}
+  a = 10;
+  return;
+  function a() {}
 }
 b2();
-console.log(a);
+console.log(a); // 1
 
+// Block 3
 let i;
 for (i = 0; i < 3; i++) {
-	const log = () => {
-		console.log(i);
-	};
-	setTimeout(log, 100);
+  const log = () => {
+    console.log(i);
+  };
+  setTimeout(log, 100);
 }
+// 3 3 3
