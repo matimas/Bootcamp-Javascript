e1068b86fd10bd2a634edae7d734ebb6d6d54c11
mati misganow <45734976+matimas@users.noreply.github.com>
2021-11-29 17:11:16 +0200

    Corrections of names and abbreviations

--- JS21.2/index.js
+++ JS21.2/index.js
@@ -1,27 +1,27 @@
-const inc = document.querySelector('.inc');
-const dec = document.querySelector('.dec');
+const incButton = document.querySelector('.inc');
+const decButton = document.querySelector('.dec');
+const FONT_SIZE_INCEMENT = 2;
 
-inc.addEventListener('click', (e) => {
-	const para = document.querySelector('p');
-	const copmuted_para = getComputedStyle(para);
-	let p_fontsize_px = copmuted_para.fontSize;
-	let p_fontsize_num = parseInt(p_fontsize_px);
-	if (p_fontsize_num < 100) {
-		let inc_fontpx = (p_fontsize_num + 2).toString() + 'px';
-		para.style.fontSize = inc_fontpx;
-	} else {
-		para.style.fontSize = '100px';
+const getFontSize = (value) => {
+	return value.toString() + 'px';
+};
+
+const changeTextSize = (delta) => {
+	const textElement = document.querySelector('p');
+	let fontSizePx = parseInt(getComputedStyle(textElement).fontSize);
+	if (fontSizePx > 100) {
+		fontSizePx = 100;
 	}
+	if (fontSizePx < 6) {
+		fontSizePx = 6;
+	}
+	textElement.style.fontSize = getFontSize(fontSizePx + delta);
+};
+
+incButton.addEventListener('click', (e) => {
+	changeTextSize(FONT_SIZE_INCEMENT);
 });
-dec.addEventListener('click', (e) => {
-	const para = document.querySelector('p');
-	const copmuted_para = getComputedStyle(para);
-	let p_fontsize_px = copmuted_para.fontSize;
-	let p_fontsize_num = parseInt(p_fontsize_px);
-	if (p_fontsize_num > 6) {
-		let inc_fontpx = (p_fontsize_num - 2).toString() + 'px';
-		para.style.fontSize = inc_fontpx;
-	} else {
-		para.style.fontSize = '6px';
-	}
+
+decButton.addEventListener('click', (e) => {
+	changeTextSize(-1 * FONT_SIZE_INCEMENT);
 });
